493eb1d4d78e8d0134f73bba8e788a9540797502
Alan Monroy <alanmonroyzz99@gmail.com>
2023-01-25 15:52:24 -0600

    JS1

---
+++ combinacion_taclas.js
@@ -0,0 +1,13 @@
+const keyBoard = document.querySelector('.keyboard')
+
+if (keyBoard){
+    keyBoard.addEventListener('keydown', e =>{
+    console.log(e)
+    if (e.keyCode === 70){
+        alert('TECLA PRESIONADA: ' + e.KEY)
+        } 
+    })
+}
+else{
+    alert("No econtrado")
+}
---
+++ console.js
@@ -0,0 +1,4 @@
+document.getElementById("New").onclick = function() {mi_funcion()};
+function mi_funcion() {
+    console.log("Adios a todos")
+}
